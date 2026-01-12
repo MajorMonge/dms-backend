b77e105ab0b582eedefd0cd1c3b1068921299466
Luiz F Monge <luizfilipemonge@outlook.com>
2026-01-12 20:35:45 -0300

    fix: Update default AWS region in configuration from us-east-1 to us-east-2

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,7 +20,7 @@ export const config = {
 
   // AWS General
   aws: {
-    region: process.env.AWS_REGION || 'us-east-1',
+    region: process.env.AWS_REGION || 'us-east-2',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },

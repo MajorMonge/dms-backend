7e5b4ee07c08912c14822d662e18d2ee5497455c
Luiz F Monge <luizfilipemonge@outlook.com>
2026-01-14 06:04:39 -0300

    fix: correct export statement to use 'type' for type exports in index.ts

--- src/adapters/database/index.ts
+++ src/adapters/database/index.ts
@@ -1,2 +1,2 @@
-export { IDatabaseAdapter, DatabaseConnectionOptions, QueryOptions, PaginatedResult } from './IDatabaseAdapter.js';
+export  type  { IDatabaseAdapter, DatabaseConnectionOptions, QueryOptions, PaginatedResult } from './IDatabaseAdapter.js';
 export { MongoDBAdapter } from './MongoDBAdapter.js';
